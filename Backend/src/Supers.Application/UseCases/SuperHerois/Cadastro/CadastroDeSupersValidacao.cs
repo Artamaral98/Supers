89ca2dd94579541afb47d93cd6b642f3fbfb0f38
Arthur Amaral <arrhur.a98@gmail.com>
2025-10-03 16:42:10 -0300

    ajuste validação de caracteres especiais

--- Backend/src/Supers.Application/UseCases/SuperHerois/Cadastro/CadastroDeSupersValidacao.cs
+++ Backend/src/Supers.Application/UseCases/SuperHerois/Cadastro/CadastroDeSupersValidacao.cs
@@ -10,10 +10,10 @@ namespace Supers.Application.UseCases.SuperHerois.Cadastro
         {
             RuleFor(user => user.Nome).NotEmpty().WithMessage(Mensagens.NOME_VAZIO);
             //Não permite caracteres especiais, impedindo SQL Injection
-            RuleFor(user => user.Nome).Matches(@"^[a-zA-ZÀ-ÿ\s]+$").WithMessage(Mensagens.NOME_CARACTERES_ESPECIAIS);
+            RuleFor(user => user.NomeHeroi).Matches(@"^[a-zA-ZÀ-ÿ\s0-9]+$").WithMessage(Mensagens.NOME_CARACTERES_ESPECIAIS);
             RuleFor(user => user.Nome).MaximumLength(120).WithMessage(Mensagens.NOME_TAMANHO_MAXIMO);
             RuleFor(user => user.NomeHeroi).NotEmpty().WithMessage(Mensagens.NOME_HEROI_VAZIO);
-            RuleFor(user => user.NomeHeroi).Matches(@"^[a-zA-ZÀ-ÿ\s]+$").WithMessage(Mensagens.NOME_CARACTERES_ESPECIAIS);
+            RuleFor(user => user.NomeHeroi).Matches(@"^[a-zA-ZÀ-ÿ\s0-9]+$").WithMessage(Mensagens.NOME_CARACTERES_ESPECIAIS);
             RuleFor(user => user.NomeHeroi).MaximumLength(120).WithMessage(Mensagens.NOME_HEROI_TAMANHO_MAXIMO);
             RuleFor(user => user.SuperPoderes).NotEmpty().WithMessage(Mensagens.SUPER_PODERES_VAZIO);
             RuleFor(user => user.DataNascimento).NotEmpty().WithMessage(Mensagens.DATA_NASCIMENTO_VAZIO);
